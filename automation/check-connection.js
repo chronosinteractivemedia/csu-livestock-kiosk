f8a844ada83f18e6399f18af92f09e3b2a55436e
Jordan Sullivan <jmsull@protonmail.com>
2025-01-29 13:30:32 -0700

    waiting 30 second on connection check

--- automation/check-connection.js
+++ automation/check-connection.js
@@ -1,11 +1,20 @@
-const http = require("https");
+const https = require("https");
 
-console.log('Checking connection...');
-http.get(`https://csu-tg-cms.onrender.com/upload/files`, (response) => {
-  if (response.statusCode === 200) {
-    console.log('Connection to CMS is intact!')
-    process.exit(0);
-  } else {
-    throw new Error('No connection detected!');
-  }
-});
+console.log("Waiting 30 seconds before checking connection...");
+
+setTimeout(() => {
+  console.log("Checking connection...");
+
+  https.get("https://csu-tg-cms.onrender.com/upload/files", (response) => {
+    if (response.statusCode === 200) {
+      console.log("Connection to CMS is intact!");
+      process.exit(0);
+    } else {
+      throw new Error("No connection detected!");
+    }
+  }).on("error", (err) => {
+    console.error("Request failed:", err.message);
+    process.exit(1);
+  });
+
+}, 30000); // 30 seconds delay
